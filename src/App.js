0f48f4a413e605912e4a896c19dfcff9aec100ed
Taisei <taisei96.web@gmail.com>
2023-06-09 13:36:09 +0900

    html雛形

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='formContainer'>
+        <form>
+          <h1>ログインフォーム</h1>
+          <hr />
+
+          <div className="uiForm">
+
+            <div className="formField">
+              <label>ユーザー名</label>
+              <input type='text' placeholder='ユーザー名' name='' />
+            </div>
+
+            <div className="formField">
+              <label>メールアドレス</label>
+              <input type='text' placeholder='メールアドレス' name='' />
+            </div>
+
+            <div className="formField">
+              <label>パスワード</label>
+              <input type='text' placeholder='パスワード' name='' />
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
